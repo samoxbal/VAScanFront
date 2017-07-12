b16cf29f87a8c731dd8a5e999a4ba9474205997e
Dmitry Samokhvalov <dsamokhvalov@alfabank.ru>
2017-07-12 17:32:36 +0300

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ module.exports = {
     entry: !isProduction ? [
       'webpack-dev-server/client?http://localhost:3002',
       'webpack/hot/only-dev-server',
-      './src/js/index'
+      './src/index'
     ] : [
-        './src/js/index'
+        './src/index'
     ],
     output: {
         path: path.join(__dirname, 'dist'),
@@ -29,8 +29,7 @@ module.exports = {
         rules: [
             {
                 test: /\.jsx?$/,
-                use: isProduction ? ['babel-loader'] : ['react-hot-loader', 'babel-loader'],
-                include: [path.join(__dirname, 'frontend/js')]
+                use: isProduction ? ['babel-loader'] : ['react-hot-loader', 'babel-loader']
             },
             {   test: /\.css$/,
                 use: ExtractTextPlugin.extract({
@@ -42,29 +41,7 @@ module.exports = {
                         },
                         { loader: 'postcss-loader' }
                     ]
-                }),
-                include: path.join(__dirname, 'frontend/assets/css')
-            },
-            {
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader",
-                include: path.join(__dirname, 'frontend/assets/fonts')
-            },
-            {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader",
-                options: {
-                    limit: '100000'
-                },
-                include: path.join(__dirname, 'frontend/assets/fonts')
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loader: "file-loader",
-                options: {
-                    limit: '100000'
-                },
-                include: path.join(__dirname, 'frontend/assets/images')
+                })
             }
         ]
     },
