ac71c0d2cb85c370b38d4ebed5e4329e557ba129
Dmitry Samokhvalov <dsamokhvalov@alfabank.ru>
2017-10-02 13:22:36 +0300

    Add billet for code splitting

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import Loadable from 'react-loadable';
+import CircularProgress from 'material-ui/CircularProgress';
 import { Route, Redirect } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
 import AddExperiment from '../pages/add-experiment/AddExperiment';
@@ -5,14 +7,22 @@ import ExperimentsPage from '../pages/experiments/ExperimentsPage';
 import VoltamogrammPage from '../pages/voltamogramm/VoltamogrammPage';
 import MeasurePage from '../pages/measure/MeasurePage';
 
-const renderLogin = () => {
-    return <Redirect to="/all" />;
-};
+const AsyncRoute = ({ ...rest }) => (
+    <Route
+        { ...rest }
+        component={
+            Loadable({
+                loader: () => import('../pages/add-experiment/AddExperiment'),
+                loading: CircularProgress
+            })
+        }
+    />
+);
 
 const App = () => (
     <Switch>
-        <Route exact path="/" render={ renderLogin } />
-        <Route path="/add" component={ AddExperiment } />
+        <Redirect to="/all" from="/" exact />
+        <AsyncRoute path="/add" />
         <Route path="/all" component={ ExperimentsPage } />
         <Route path="/voltamogramm/:id" component={ VoltamogrammPage } />
         <Route path="/measure/:id" component={ MeasurePage } />
